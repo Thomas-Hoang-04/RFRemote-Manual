1007c98acb0099a771dc6da10cef6d1382be4a52
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2025-03-30 00:57:43 +0700

    feat: add screens for Setting mode

--- OLED_Display.ino
+++ OLED_Display.ino
@@ -3,12 +3,12 @@
 #include <Adafruit_SSD1306.h>
 #include "lib.h"
 
-// OLED display specifications and pinouts
+// OLED display specifications and pinouts (SSD1306 128x64 OLED 0.96')
 #define WIDTH 128
 #define HEIGHT 64
 #define OLED_RST -1
-#define SDA 4
-#define SCL 5
+#define SDA 4 // GPIO4 (D2 on NodeMCU)
+#define SCL 5 // GPIO5 (D1 on NodeMCU)
 
 // Initialize the display interface
 Adafruit_SSD1306 display(WIDTH, HEIGHT, &Wire, OLED_RST);
@@ -17,8 +17,8 @@ Adafruit_SSD1306 display(WIDTH, HEIGHT, &Wire, OLED_RST);
 void OLED_Setup() {
   Wire.begin(SDA, SCL);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("OLED allocation failed"));
-    for (;;);
+    delay(1500);
+    ESP.restart();
   }
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -90,4 +90,77 @@ void menu_state(int selected) {
     display.println(options[i]);
   }
 }
+
+void cursor(int selected) {
+  int sel_opt = NUM_X_START + (BOX_WIDTH + SPACE) * selected;
+  display.fillRect(sel_opt, NUM_Y_START + CHAR_HEIGHT, BOX_WIDTH, BOX_HEIGHT, WHITE);
+  display.display();
+  delay(500);
+  display.fillRect(sel_opt, NUM_Y_START + CHAR_HEIGHT, BOX_WIDTH, BOX_HEIGHT, BLACK);
+  display.display();
+  delay(500);
+}
+
+void chn_cursor() {
+  int st = curr_chn ? 98 : 113;
+  const char* curr_chn_str = curr_chn ? "2" : "4";
+  display.fillRect(st, 49, 9, 10, WHITE);
+  display.setCursor(st + 2, 50);
+  display.setTextColor(BLACK);
+  display.print(curr_chn_str);
+  display.display();
+  delay(500);
+  display.fillRect(st, 49, 9, 10, BLACK);
+  display.setCursor(st + 2, 50);
+  display.setTextColor(WHITE);
+  display.print(curr_chn_str);
+  display.display();
+  delay(500);
+}
+
+void setting_menu(int selected) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(7, 10);
+  display.println("Enter transmit code");
+  display.setTextSize(2);
+  int sel_opt = NUM_X_START + (BOX_WIDTH + SPACE) * selected;
+  for (int i = 0; i < 8; i++) {
+    int st = NUM_X_START + (BOX_WIDTH + SPACE) * i;
+    display.setCursor(st, NUM_Y_START);
+    display.print(curr_code[i]);
+  }
+  display.fillRect(sel_opt, NUM_Y_START + CHAR_HEIGHT, BOX_WIDTH, BOX_HEIGHT, WHITE);
+  display.setTextSize(1);
+  display.setCursor(4, 50);
+  display.print("Select channel:");
+  selected_chn();
+
+  display.display();
+}
+
+void selected_chn() {
+  int st = curr_chn ? 98 : 113;
+  display.fillRect(st, 49, 9, 10, WHITE);
+  display.setTextSize(1);
+  display.setTextColor(curr_chn ? BLACK : WHITE);
+  display.setCursor(100, 50);
+  display.print("2");
+  display.setTextColor(curr_chn ? WHITE : BLACK);
+  display.setCursor(115, 50);
+  display.print("4");
+}
+
+void display_saved(bool state) {
+  const char* prompt = state ? "Configuration Saved!" : "Configuration Failed!";
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(6, 30);
+  display.setTextColor(WHITE);
+  display.println(prompt);
+  display.display();
+}
+
+
 
